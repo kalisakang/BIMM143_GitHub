34b50658c286fa99f42514686eaaa65b180f69ef
Kalisa Kang <k4kang@ucsd.edu>
2024-05-21 11:56:49 -0700

    Add Class04

---
+++ Class04/Class04.R
@@ -0,0 +1,12 @@
+#' ---
+#' title: "20240411 BIMM143 Lab 4"
+#' author: "Kalisa Kang (PID: A16741690)"
+#' ---
+
+
+# Text file
+x <- 1:50
+plot(x, sin(x))
+plot(x,sin(x), type = "l", col="purple", lwd=3)
+
+
